d16e75f0f24538651957ce80f324580873c7415e
Giulio Porcu <giulioporcu@outlook.com>
2025-07-31 18:43:33 +0200

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Portfolio.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Portfolio.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
