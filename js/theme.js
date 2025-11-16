d08ed48cf93d20372cfafc7bc032cb1dabe6e103
GiulioPorcu <GiulioPorcu@users.noreply.github.com>
2025-11-16 08:16:49 +0000

    deploy: 523488b9bd4d0911d4aa5d16546f0f0dc7084111

---
+++ js/theme.js
@@ -0,0 +1,27 @@
+﻿const dataThemeAttribute = 'data-theme';
+
+const darkTheme = 'dark';
+const lightTheme = 'light';
+
+/**
+ * Toggles the system theme by changing the value of the data-theme attribute.
+ */
+window.toggleTheme = () => {
+    const html = document.documentElement;
+    const current = html.getAttribute(dataThemeAttribute);
+    const next = current === darkTheme ? lightTheme : darkTheme;
+
+    html.setAttribute(dataThemeAttribute, next);
+    localStorage.setItem(dataThemeAttribute, next);
+};
+
+/**
+ * Applies the data-theme attribute, if present.
+ */
+window.applyTheme = () => {
+    const stored = localStorage.getItem(dataThemeAttribute);
+
+    if (stored) {
+        document.documentElement.setAttribute(dataThemeAttribute, stored);
+    }
+};
